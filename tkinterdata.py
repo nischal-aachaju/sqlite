1d42baccf80676412ece8f6f625be37fdf9e4b61
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-28 14:17:46 +0545

    update

--- tkinterdata.py
+++ tkinterdata.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 
+import data
+
 root=Tk()
 root.title("DATABASE")
 root.geometry("400x400")
@@ -70,28 +72,54 @@ def delete():
     conn.close()
     query()
 
+def update():
+    conn=sqlite3.connect("student_records.db")
+    cur=conn.cursor()
+    data= (f_name_editor.get(),l_name_editor.get(),address_editor.get(),delete_box.get())
+    cur.execute("UPDATE student SET first_name=?, last_name=?, address=? Where oid=?",data)
+    conn.commit()
+    conn.close()
+
 def edit():
+    
+    if not delete_box.get():
+        messagebox.showwarning("input error","Please enter ID")
+        return
+    
+    global top_editor
     top_editor=Tk()
     top_editor.title("DATABASE")
     top_editor.geometry("600x400")
+    top_editor.config(bg="blue")
+    conn=sqlite3.connect("student_records.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM student WHERE oid="+ delete_box.get())
+    print("Edit Successfully")
     
-    f_name=Entry(top_editor,width=30,bg="#2c3e50",fg="#ecf0f1",)
-    f_name.grid(row=0,column=1,padx=20,pady=(10,0),ipady=5)
-    l_name=Entry(top_editor,width=30,bg="#2c3e50",fg="#ecf0f1",)
-    l_name.grid(row=1,column=1,ipady=5)
-    address=Entry(top_editor,width=30,bg="#2c3e50",fg="#ecf0f1",)
-    address.grid(row=2,column=1,ipady=5)
+    records=cur.fetchall()
 
-    f_name_label=Label(top_editor,text="First name :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
-    f_name_label.grid(row=0,column=0,pady=(10,0))
 
-    l_name_label=Label(top_editor,text="Last name :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
-    l_name_label.grid(row=1,column=0,pady=5)
 
-    address_label=Label(top_editor,text="Address :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
-    address_label.grid(row=2,column=0,pady=5)
+    global f_name_editor,l_name_editor,address_editor
+    f_name_editor=Entry(top_editor,width=30)
+    f_name_editor.grid(row=0,column=1,padx=20,pady=(10,0),ipady=5)
+    l_name_editor=Entry(top_editor,width=30)
+    l_name_editor.grid(row=1,column=1,ipady=5)
+    address_editor=Entry(top_editor,width=30)
+    address_editor.grid(row=2,column=1,ipady=5)
+
+    f_name_label_editor=Label(top_editor,text="First name :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
+    f_name_label_editor.grid(row=0,column=0,pady=(10,0))
+    l_name_label_editor=Label(top_editor,text="Last name :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
+    l_name_label_editor.grid(row=1,column=0,pady=5)
+    address_label_editor=Label(top_editor,text="Address :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
+    address_label_editor.grid(row=2,column=0,pady=5)
+    for record in records:
+        f_name_editor.insert(0,record[0])
+        l_name_editor.insert(0,record[1])
+        address_editor.insert(0,record[2])
     
-    edit_btn=Button(top_editor,text="save" ,bg="#7538c0", fg="#ecf0f1",height=2 )
+    edit_btn=Button(top_editor,text="save" ,bg="#7538c0", fg="#ecf0f1",height=2,command=update )
     edit_btn.grid(row=4,column=0,columnspan=2,pady=10,padx=1,ipadx=100)
 
 
@@ -117,8 +145,10 @@ l_name_label.grid(row=1,column=0,pady=5)
 address_label=Label(root,text="Address :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
 address_label.grid(row=2,column=0,pady=5)
 
-delete_label=Label(root,text="Delete ID :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
+delete_label=Label(root,text="ID :",bg="#2c3e50",fg="#ecf0f1",font=("Segoe UI",9,"bold"))
 delete_label.grid(row=3,column=0,pady=(10,0))
+
+
 
 
 submit_btn=Button(root,text="Add Records" ,bg="#7538c0", fg="#ecf0f1",height=2 ,command=submit)
@@ -128,12 +158,12 @@ query_btn=Button(root,text="Show Records" ,bg="#7538c0", fg="#ecf0f1",height=2 ,
 query_btn.grid(row=5,column=0,columnspan=2,pady=10,padx=10,ipadx=100)
 
 # delete_btn=Button(root,text="Delete ID" ,bg="#7538c0", fg="#ecf0f1",height=2 )
-# delete_btn.grid(row=6,column=0,columnspan=2,pady=10,padx=10,ipadx=100)
+# delete_btn.grid(row=6,column=0,columnspan=2,pady=10,padx=10,ipadx=100 )
 
 delete_btn=Button(root,text="Delete" ,bg="#7538c0", fg="#ecf0f1",height=2 ,command=delete)
 delete_btn.grid(row=6,column=0,columnspan=2,pady=10,padx=1,ipadx=100)
 
-edit_btn=Button(root,text="Edit" ,bg="#7538c0", fg="#ecf0f1",height=2 ,command=edit)
+edit_btn=Button(root,text="Update" ,bg="#7538c0", fg="#ecf0f1",height=2 ,command=edit)
 edit_btn.grid(row=7,column=0,columnspan=2,pady=10,padx=1,ipadx=100)
 
 # data_box=Label()
